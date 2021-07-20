f65bd4d08de90e30433c2cb58b6e5c67a38c99de
Eumin Hong <milouhong45@gmail.com>
2021-07-20 00:30:19 -0400

    pdflatex error handling

--- compile.sh
+++ compile.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 cd output/
-pdflatex doc.tex
-pdfcrop doc.pdf
-pdftoppm -r 1000 doc-crop.pdf | pnmtopng > output.png
+pdflatex -halt-on-error -interaction=nonstopmode doc.tex
+if [ $? -eq 0 ]; then
+    echo OK
+    pdfcrop doc.pdf
+    pdftoppm -r 1000 doc-crop.pdf | pnmtopng > output.png
+else
+    echo FAIL
+fi
 rm doc*
 cd ../
 echo 'done'
